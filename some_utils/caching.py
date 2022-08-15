5880ed7f1b989d326e79e8e3a8401f6eeaa12e44
Teshan Liyanage <teshanuka@gmail.com>
2022-08-15 22:26:51 +0200

    Add simple caching

---
+++ some_utils/caching.py
@@ -0,0 +1,27 @@
+import pickle
+import os
+from functools import wraps
+
+
+def simple_cache(file):
+    def decor(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if os.path.exists(file):
+                with open(file, 'rb') as fd:
+                    ret = pickle.load(fd)
+                return ret
+            ret = func(*args, **kwargs)
+            with open(file, 'wb') as fd:
+                pickle.dump(ret, fd)
+            return ret
+        return wrapper
+    return decor
+
+
+if __name__ == '__main__':
+    @simple_cache('tmp.pkl')
+    def mult(a, b):
+        return a*b
+
+    print(mult(2, 5))
