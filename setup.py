34c8bac59880996fff9ed20355338d5362f58c15
Teshan Liyanage <teshan.shanuka@cm-robotics.com>
2024-08-21 14:16:35 +0200

    Add pkg version

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ with open("README.md") as fp:
 with open("requirements.txt") as fp:
     install_requires = fp.read().strip().splitlines()
 
+with open("some_utils/__init__.py") as fd:
+    exec("".join(l for l in fd if l.startswith("__")))
+
 setuptools.setup(
     name="some_utils",  # Some name for the package
-    author="Teshan Liyanage",
+    version=__version__,
+    author=__author__,
     author_email="teshanuka@gmail.com",
-    description="Some useful utilities by Teshan Liyanage",
+    description="Some useful utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=['some_utils'],
