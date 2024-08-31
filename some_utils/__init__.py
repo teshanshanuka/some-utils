cc185e45b3782f2b7338f63f4a756b8077d3587e
Teshan Liyanage <teshan.shanuka@cm-robotics.com>
2024-08-31 10:19:59 +0200

    Release 0.0.1

--- some_utils/__init__.py
+++ some_utils/__init__.py
@@ -1,5 +1,5 @@
 # Author: Teshan Liyanage <teshanuka@gmail.com>
 
-__version_info__ = ('0', '0', '0b1')
+__version_info__ = ('0', '0', '1')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Teshan Liyanage <teshanuka@gmail.com>'
